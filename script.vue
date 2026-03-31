6024259c1ce7ab9a0acd1251a633cbf8fce25a95
PabloMartinsBorges <51960948+PabloMartinsBorges@users.noreply.github.com>
2026-03-31 01:37:02 -0300

    script básico do vue 3

---
+++ script.vue
@@ -0,0 +1,14 @@
+// Importando as funções necessárias do Vue 3
+const { createApp, ref, computed } = Vue;
+
+const app = createApp({
+  setup() {
+    const transacoes = ref([]); 
+    const novaDescricao = ref(''); 
+    const novoValor = ref('');
+    
+    const paginaAtual = ref(1);
+    const itensPorPagina = 5;
+
+  }
+}).mount('#app'); // Monta a aplicação na div principal
