b7eafc64afd54d1dd74d148bd44151dabf8b9c17
PabloMartinsBorges <51960948+PabloMartinsBorges@users.noreply.github.com>
2026-04-02 18:54:09 -0300

    add total de páginas e adicionar uma transação

--- script.vue
+++ script.vue
@@ -10,5 +10,27 @@ const app = createApp({
     const paginaAtual = ref(1);
     const itensPorPagina = 5;
 
+
+    const totalPaginas = computed(() => {
+      return Math.ceil(transacoes.value.length / itensPorPagina);
+    });
+
+    const adicionarTransacao = () => {
+      if (!novaDescricao.value || !novoValor.value) return;
+
+      transacoes.value.push({
+        descricao: novaDescricao.value,
+        valor: parseFloat(novoValor.value)
+      });
+
+      // Limpa o formulário após adicionar
+      novaDescricao.value = '';
+      novoValor.value = '';
+      
+    };
+
+
+
   }
+  
 }).mount('#app'); // Monta a aplicação na div principal
